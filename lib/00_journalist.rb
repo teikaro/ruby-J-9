8b4540fef4d10338b36a166e1602e7129c3b6d87
teikaro <48406803+teikaro@users.noreply.github.com>
2021-04-15 23:14:33 +0200

    Add files via upload

---
+++ lib/00_journalist.rb
@@ -0,0 +1,130 @@
+handles = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+def array_length(array)
+  how_many_handle = array.length
+  puts "Dans cet array, il y'a #{how_many_handle} handles Twitter."
+end
+
+def shorter_handle(array)
+  index = 0
+  index_size = 100
+  index_shorter = 0
+  array.each do |handle|
+    size = handle.length
+    if index_size > size
+      index_size = size
+      index_shorter = index
+    end
+    index += 1
+  end
+  puts "Le handle le plus court de cette liste est #{array[index_shorter]} à l'index : #{index_shorter}."
+end
+
+def five_char_handles(array)
+  five_char = []
+  array.each do |handle|
+    size = handle.length - 1
+    if size == 5
+      five_char.push(handle)
+    end
+  end
+  puts "Il y'a #{five_char.length} handles contenant 5 caractères."
+end
+
+def uppercase_handles(array)
+  upper_items = []
+  array.each do |handle|
+    if handle[1] == handle[1].upcase
+      upper_items.push(handle)
+    end
+  end
+  puts "Il y'a #{upper_items.length} handles commençant par une lettre majuscule."
+end
+
+def alphabet_sort(array)
+  ordered_handles = array.sort_by(&:downcase)
+  puts "Voici les handles triés par ordre alphabétique indépendamment des majuscules :"
+  puts ordered_handles
+end
+
+def min_to_max(array)
+  ordered_handles = array.sort_by(&:length)
+  puts "Voici les handles triés par taille :"
+  puts ordered_handles
+end
+
+def find_epenser(array)
+  index = array.find_index("@epenser")
+  puts "L'index d'#{array[index]} est #{index} (Il suffisait d'e-penser..)."
+end
+
+def handles_size(array)
+  handles_sizes_sort = {}
+  ordered_handles = array.sort_by(&:length)
+  min_handle_size = ordered_handles[0].length - 1
+  max_handle_size = ordered_handles[-1].length - 1
+  for x in (min_handle_size..max_handle_size)
+    handles_sizes_sort[x] = 0
+  end
+  array.each do |handle|
+    handles_sizes_sort[handle.length - 1] += 1
+  end
+  handles_sizes_sort.each do |size, nbr|
+    if nbr > 0
+      puts "Il y'a #{nbr} handle de #{size} caractères"
+    end
+  end
+end
+
+def perform(array)
+  puts "Bienvenue dans Mega Big Data De La Mort Qui Tue aka MBDDLMQT !"
+  puts "A partir d'une liste de handle Twitter, et d'un peu d'huile de coude, mon associée et moi, nous avons pû répondre à des questions en lien avec la Data Science !"
+  puts "Vous êtes actuellement dans le menu de ce programme, à partir duquel vous allez pouvoir choisir les questions qui vous intéressent."
+  message_gentil = ["Passez une bonne journée !", "Bonne correction !", "T'es bg", "Vous avez un beau sourire !", "Vous êtes un Ruby !"]
+  menu_on = true
+  while menu_on
+    puts " "
+    puts "Voici les différentes options, le numéro qui leur est attribué sera celui à taper si vous voulez voir la réponse."
+    puts "[0] Quitter le programme."
+    puts "[1] Si vous voulez voir la liste de base."
+    puts "[2] Si vous voulez savoir combien il y'a de handle dans ce array."
+    puts "[3] Si vous voulez savoir quel est le handle le plus court."
+    puts "[4] Si vous voulez savoir combien de handle contiennent 5 caractères."
+    puts "[5] Si vous voulez savoir combien de handle commencent par une majuscule."
+    puts "[6] Si vous voulez voir la liste triée par ordre alphabétique."
+    puts "[7] Si vous voulez voir la liste triée du handle le plus court au plus long."
+    puts "[8] Si vous voulez connaître la position dans la liste d'@epenser."
+    puts "[9] Si vous voulez voir la répartition des handles par taille en caractères."
+    puts "[10] Si vous voulez un petit message gentil !"
+    puts "Entrez votre choix :"
+    print "> "
+    choice = gets.chomp
+    if choice == "0"
+      menu_on = false
+    elsif choice == "1"
+      puts array
+    elsif choice == "2"
+      array_length(array)
+    elsif choice == "3"
+      shorter_handle(array)
+    elsif choice == "4"
+      five_char_handles(array)
+    elsif choice == "5"
+      uppercase_handles(array)
+    elsif choice == "6"
+      alphabet_sort(array)
+    elsif choice == "7"
+      min_to_max(array)
+    elsif choice == "8"
+      find_epenser(array)
+    elsif choice == "9"
+      handles_size(array)
+    elsif choice == "10"
+      puts message_gentil.sample
+    else
+      puts "Vous n'avez pas entré un bon numéro, réessayez."
+    end
+  end
+end
+
+perform(handles)
